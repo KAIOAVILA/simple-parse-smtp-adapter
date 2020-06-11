d0970a1455b687bb9d488428796c1c1bcf010a6f
Kaio Ávila <21.KAIO@GMAIL.COM>
2020-06-11 15:21:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ let SimpleParseSmtpAdapter = (adapterOptions) => {
             pass: adapterOptions.password
         },
         tls: {
-            ciphers: adapterOptions.ciphers !== undefined ? adapterOptions.ciphers : undefined,
             rejectUnauthorized: adapterOptions.isTlsRejectUnauthorized !== undefined ? adapterOptions.isTlsRejectUnauthorized : true
         }
     });
